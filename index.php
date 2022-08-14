50e56034e63c6a31028a82ebf1c072150e9e5570
Abhishek Puri <avishekp@my.com>
2022-08-14 22:25:10 +0530

    Jumbotron Improvement

--- index.php
+++ index.php
@@ -8,6 +8,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+    <script type="text/css">
+        .pre {
+            display: block;
+            unicode-bidi: embed;
+            font-family: monospace;
+            white-space: pre;
+        }
+    </script>
 </head>
 <?php
 $url = './response.json';
@@ -19,12 +27,12 @@ $jsonObject = json_decode($json, true);
 <div class="container">
     <!-- Header -->
     <div class="p-5 mb-4 bg-light rounded-3">
-        <div class="container-fluid py-5">
+        <div class="container-fluid py-5 mt-5">
             <h1 class="display-1">
                 Durga Kawach
             </h1>
             <code class="fs-3">
-                <pre><?php echo $jsonObject['durgaKawach']; ?></pre>
+                <span class="pre"><?php echo $jsonObject['durgaKawach']; ?></span>
             </code>
         </div>
     </div>
